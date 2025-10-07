857a4b392708359ce425834a13be17b309a1d804
AnthonyG8520 <anthony.j.gonzales22@gmail.com>
2025-10-07 10:43:56 -0500

    form built reactively using form group / handling submit with ngSubmit and function in ts file"

--- src/app/user-form/user-form.ts
+++ src/app/user-form/user-form.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
+// import { ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-user-form',
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './user-form.html',
   standalone: true,
   styleUrl: './user-form.css'
@@ -17,6 +18,17 @@ export class UserForm {
   }
   //this function shows getting form control values / called in the html
   //with a button / can be accessed other ways
+
+  jobInfoForm = new FormGroup({
+    companyName: new FormControl(''),
+    companyRole: new FormControl('')
+  })
+
+  handleSubmit(){
+    alert(this.jobInfoForm.value.companyName + ' | ' + this.jobInfoForm.value.companyRole)
+  }
+  //this function shows handling the submit and accessing the
+  //values of the form through the form group reactively
 
 
 }
